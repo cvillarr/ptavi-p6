806ae4b85b3fbb04592bfd5296a670d2645d9370
cvillarr <c.villarr@alumnos.urjc.es>
2018-11-20 11:59:30 +0100

    mp32rtp no funciona

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+import os
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -23,7 +25,9 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             self.wfile.write(b"SIP/2.0 100 Trying\r\nSIP/2.0 180 Ringing\r\n"
                              + b"SIP/2.0 200 OK\r\n")
         elif line_conten[0] == "ACK":
-            self.wfile.write(b"ACK recibido")
+            aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 <' + ARCHIVO
+            print("Vamos a ejecutar", aEjecutar)
+            os.system(aEjecutar)
         elif line_conten[0] == "BYE":
             self.wfile.write(b"Finalizando comunicacion")
         if line_conten[0] != "BYE":
@@ -35,7 +39,13 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
 
 if __name__ == "__main__":
+    try:
+        PORT = int(sys.argv[2])
+        ARCHIVO = sys.argv[3]
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 5555), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+        serv = socketserver.UDPServer(('', PORT), EchoHandler)
+        print("Listening...")
+        serv.serve_forever()
+    except IndexError:
+        sys.exit("Usage: python3 server.py IP port audio_file")
+    
